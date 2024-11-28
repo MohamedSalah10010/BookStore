5693ce24eb15670291c84d2102b387317fcfdee1
MohamedSalah10010 <abosalah100001000@gmail.com>
2024-11-28 18:40:53 +0200

    update AdminDTO and insert some empty controllers

--- BookStore/DTOs/adminDTO/addAdminDTO.cs
+++ BookStore/DTOs/adminDTO/addAdminDTO.cs
@@ -8,13 +8,15 @@ namespace BookStore.DTOs.adminDTO
         [Required]
         public string Username { get; set; }
         [Required]
+        [RegularExpression("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$")]
+
         public string Password { get; set; }
         [Required]
+        [RegularExpression("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")]
 
         public string Email { get; set; }
         [Required]
 
-        public string Phonenumber
-        { get; set; }
+        public string Phonenumber  { get; set; }
     }
 }
